1f6fdbbb2edd5900528aa6e115f401751f0c8960
Fernando <fernandocazon2@gmail.com>
2025-02-21 09:24:45 -0300

    fix: deploy fix

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,38 +1,37 @@
 import { CloseButton, CreditBox, CreditContainer, Header, LearnMoreButton, ModalContent, ModalWrapper, Tab, Tabs, ToggleContainer, ToggleSwitch } from "./styles";
 
-export const Modal = ({ onClose }) => {
-return (
-  <ModalWrapper>
-    <ModalContent>
-      <Header>
-        <span>GREEN SAVER</span>
-        <CloseButton onClick={onClose}>×</CloseButton>
-      </Header>
+export const Modal = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <Header>
+          <span>GREEN SAVER</span>
+          <CloseButton onClick={onClose}>×</CloseButton>
+        </Header>
 
-      <Tabs>
-        <Tab>Your Credits</Tab>
-        <Tab>Dashboard</Tab>
-      </Tabs>
+        <Tabs>
+          <Tab>Your Credits</Tab>
+          <Tab>Dashboard</Tab>
+        </Tabs>
 
-      <CreditContainer>
-        <CreditBox>
-          22 <br /> Tokens
-        </CreditBox>
-        <CreditBox>
-          2500 <br /> Credits
-        </CreditBox>
-      </CreditContainer>
+        <CreditContainer>
+          <CreditBox>
+            22 <br /> Tokens
+          </CreditBox>
+          <CreditBox>
+            2500 <br /> Credits
+          </CreditBox>
+        </CreditContainer>
 
-      <ToggleContainer>
-        <span>Efficiency Mode</span>
-        <ToggleSwitch />
-      </ToggleContainer>
+        <ToggleContainer>
+          <span>Efficiency Mode</span>
+          <ToggleSwitch />
+        </ToggleContainer>
 
-      <p>Learn how to use your credits</p>
-      <LearnMoreButton>Learn More</LearnMoreButton>
-    </ModalContent>
-  </ModalWrapper>
-);
-};
-
+        <p>Learn how to use your credits</p>
+        <LearnMoreButton>Learn More</LearnMoreButton>
+      </ModalContent>
+    </ModalWrapper>
+  )
+}
 export default Modal;
